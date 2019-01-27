850c6b9e47fab08f89ae8241bd0a021ed3b4fb57
Justine Nguyen <justinexnguyen@gmail.com>
2019-01-27 12:02:19 -0500

    Route to index and create view for Reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,7 @@
 class ReviewsController < ApplicationController
+    def index
+        # this is our list page for our reviews
+        @reviews = ["Zak's Diner","Blue Cactus","Fat Joe's","Weedle Taste of Europe","El Warehouse", "Benny's Bistro"]
+    end
+
 end
