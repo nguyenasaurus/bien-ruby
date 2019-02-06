bd6f5e65e5fe5ce22b5235c58528102de785174b
Justine Nguyen <justinexnguyen@gmail.com>
2019-02-06 15:14:09 -0500

    Def create for review controller with set up for view

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,31 @@
 class ReviewsController < ApplicationController
     def index
         # this is our list page for our reviews
-        @reviews = ["Zak's Diner","Blue Cactus","Fat Joe's","Weedle Taste of Europe","El Warehouse", "Benny's Bistro"]
+        @reviews = Review.all
     end
 
     def new
+        #form for adding a new review
         @review = Review.new
+    end
+
+    def create
+        #take info from form and add it to database
+        @review = Review.new(params.require(:review).permit(:restaurant, :title, :body, :score))
+
+        #save to database
+        @review.save
+
+        #redirect to homepage
+        redirect_to root_path
     end
 
 
 end
+
+# t.string :title
+# t.text :body
+# t.integer :score
+# t.string :restaurant
+# t.integer :price
+# t.string :cuisine
